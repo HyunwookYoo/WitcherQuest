402dd1dfadec0aca67348845fc27be66b8f74a93
HyunwookYoo <hyunwook5636@gmail.com>
2021-04-19 20:26:17 +0900

    Start making Witcher Quest game

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    string[] daysOfTheWeek = { "mon", "tues", "wed", "thur" };
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
